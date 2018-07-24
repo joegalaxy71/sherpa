c43da664c8585225d83a83dcbfbc5bed11869888
joegalaxy <github@simonebaglioni.com>
2018-07-24 17:03:53 +0200

    moved commands to commands.go

---
+++ commands.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	gnatsd "github.com/nats-io/gnatsd/server"
+	"github.com/nats-io/nats"
+	"github.com/spf13/cobra"
+)
+
+func history(cmd *cobra.Command, args []string) {
+	//var args_empty = []string{""}
+
+	fmt.Print("reached history\n")
+
+	// NATS client, used in daemon mode
+	// create NATS netchan (these are native go channels binded to NATS send/receive)
+	// following go idiom: "don't communicate by sharing, share by communicating"
+	nc, _ := nats.Connect(nats.DefaultURL)
+	ec, _ := nats.NewEncodedConn(nc, nats.JSON_ENCODER)
+	defer ec.Close()
+	commandCh := make(chan *command)
+	ec.BindSendChan("commands", commandCh)
+	responseCh := make(chan *response)
+	ec.BindRecvChan("responses", responseCh)
+
+	log.Notice("history: sending NATS test message")
+
+	commandCh <- &command{"Me", "Test", "args"}
+
+	// wait for all the goroutines to end before exiting
+	// (should never exit) (exit only with signal.interrupt)
+	wg.Add(1)
+	wg.Wait()
+}
+
+func daemonize(cmd *cobra.Command, args []string) {
+	//var args_empty = []string{""}
+
+	fmt.Print("reached daemonize\n")
+
+	// embedded NATS server
+	// Create the gntsd with default options (empty type)
+	var opts = gnatsd.Options{}
+	s := gnatsd.New(&opts)
+
+	// Configure the logger based on the flags
+	s.ConfigureLogger()
+
+	go gnatsd.Run(s)
+
+	// NATS client, used in daemon mode
+	// create NATS netchan (these are native go channels binded to NATS send/receive)
+	// following go idiom: "don't communicate by sharing, share by communicating"
+	nc, _ := nats.Connect(nats.DefaultURL)
+	ec, _ := nats.NewEncodedConn(nc, nats.JSON_ENCODER)
+	defer ec.Close()
+	commandCh := make(chan *command)
+	ec.BindRecvChan("commands", commandCh)
+	responseCh := make(chan *response)
+	ec.BindSendChan("responses", responseCh)
+
+	// launch a goroutine to fetch commands (they arrive via netchan)
+	// we use wg.Add(1) to add to the waitgroup so we can wait for all goroutines to end
+	// it obviously exits if we explicitly call os.exit
+	wg.Add(1)
+	go listenAndReply(commandCh, responseCh)
+
+	// wait for all the goroutines to end before exiting
+	// (should never exit) (exit only with signal.interrupt)
+	wg.Wait()
+}
--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	_ "flag"
 	"fmt"
-	gnatsd "github.com/nats-io/gnatsd/server"
-	"github.com/nats-io/nats"
 	"github.com/op/go-logging"
 	"github.com/spf13/cobra"
 	"os"
@@ -50,20 +48,20 @@ func main() {
 		Run:   daemonize,
 	}
 
-	var cmdPrint = &cobra.Command{
-		Use:   "print [string to print]",
-		Short: "Print anything to the screen",
-		Long:  "print is for printing anything back to the screen. For many years people have printed back to the screen.",
-		Args:  cobra.MinimumNArgs(1),
-		Run:   daemonize,
-	}
-
 	var cmdHistory = &cobra.Command{
 		Use:   "history",
 		Short: "Get the sherpa collected history",
 		Long:  "Sherpa gets history from all the sources available. This commands let you walk in the history.",
 		Args:  cobra.MinimumNArgs(0),
 		Run:   history,
+	}
+
+	var cmdPrint = &cobra.Command{
+		Use:   "print [string to print]",
+		Short: "Print anything to the screen",
+		Long:  "print is for printing anything back to the screen. For many years people have printed back to the screen.",
+		Args:  cobra.MinimumNArgs(1),
+		Run:   daemonize,
 	}
 
 	var cmdEcho = &cobra.Command{
@@ -97,69 +95,4 @@ func main() {
 
 }
 
-func history(cmd *cobra.Command, args []string) {
-	//var args_empty = []string{""}
-
-	fmt.Print("reached history\n")
-
-	// NATS client, used in daemon mode
-	// create NATS netchan (these are native go channels binded to NATS send/receive)
-	// following go idiom: "don't communicate by sharing, share by communicating"
-	nc, _ := nats.Connect(nats.DefaultURL)
-	ec, _ := nats.NewEncodedConn(nc, nats.JSON_ENCODER)
-	defer ec.Close()
-	commandCh := make(chan *command)
-	ec.BindSendChan("commands", commandCh)
-	responseCh := make(chan *response)
-	ec.BindRecvChan("responses", responseCh)
-
-	log.Notice("history: sending NATS test message")
-
-	commandCh <- &command{"Me", "Test", "args"}
-
-	// wait for all the goroutines to end before exiting
-	// (should never exit) (exit only with signal.interrupt)
-	wg.Add(1)
-	wg.Wait()
-}
-
-func daemonize(cmd *cobra.Command, args []string) {
-	//var args_empty = []string{""}
-
-	fmt.Print("reached daemonize\n")
-
-	// embedded NATS server
-	{
-		// Create the gntsd with default options (empty type)
-		var opts = gnatsd.Options{}
-		s := gnatsd.New(&opts)
-
-		// Configure the logger based on the flags
-		s.ConfigureLogger()
-
-		go gnatsd.Run(s)
-	}
-
-	// NATS client, used in daemon mode
-	// create NATS netchan (these are native go channels binded to NATS send/receive)
-	// following go idiom: "don't communicate by sharing, share by communicating"
-	nc, _ := nats.Connect(nats.DefaultURL)
-	ec, _ := nats.NewEncodedConn(nc, nats.JSON_ENCODER)
-	defer ec.Close()
-	commandCh := make(chan *command)
-	ec.BindRecvChan("commands", commandCh)
-	responseCh := make(chan *response)
-	ec.BindSendChan("responses", responseCh)
-
-	// launch a goroutine to fetch commands (they arrive via netchan)
-	// we use wg.Add(1) to add to the waitgroup so we can wait for all goroutines to end
-	// it obviously exits if we explicitly call os.exit
-	wg.Add(1)
-	go listenAndReply(commandCh, responseCh)
-
-	// wait for all the goroutines to end before exiting
-	// (should never exit) (exit only with signal.interrupt)
-	wg.Wait()
-}
-
-//TODO: refactor sherpa.go into main.go
+//TODO: refactor main.go into main.go
